6c57def6f0dd3265be377613be2df03f65d47bcb
HelloWorld-Iris <18535255761@163.com>
2018-10-01 12:10:29 +0800

    first commit

---
+++ MyWeather/app/src/main/java/com/proj/zyr/myweather/MainActivity.java
@@ -0,0 +1,11 @@
+package com.proj.zyr.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
